ee0a598f5fb20b6fcc9cf954987d043de1239d06
marie-hotchkiss <mtknowlton67@gmail.com>
2020-01-24 18:59:28 -0800

    finished Step 1

--- README.md
+++ README.md
@@ -68,7 +68,7 @@ an object having a graphical representation that can be displayed on the screen 
 
 Your finished project must include all of the following requirements:
 
-* [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTML file itself.
+* [x ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTML file itself.
 
 * [ ] Following the instructions in the `Header/index.js` file, create the Header component. 
 
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,40 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+//New Element
+const header = document.createElement("div");
+const date = document.createElement("span");
+const  headerh1 = document.createElement("h1");
+const  temp = document.createElement("span");
+
+// CLASS NAMES
+
+header.classList.add("header");
+date.classList.add('date');
+temp.classList.add("temp");
+
+//Text Content
+
+date.textContent = "MARCH 28, 2019";
+headerh1.textContent = "Lambda Times";
+temp.textContainer = "98°";
+
+// Structure Elements
+
+header.appendChild(date);
+header.appendChild(headerh1);
+header.appendChild(temp);
+    return header
+}
+
+
+//  created the .headerContainer here
+ 
+const headerContainer = document.querySelector(".header-container");
+ 
+console.log(headerContainer);
+
+// Here is where i appended header to headerContainer div
+headerContainer.appendChild(Header());
